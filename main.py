db8c082be33399d6931ab715fd63c33abe7b6eae
peter <pjkrupa@proton.me>
2025-06-13 12:46:11 +0200

    adds data_split function

--- main.py
+++ main.py
@@ -86,7 +86,10 @@ def go(config: DictConfig):
                 f"{config['main']['repository']}#components/train_val_test_split",
                 'main',
                 parameters = {
-                        ...
+                        'input': 'clean_sample.csv:latest',
+                        'test_size': config['modeling']['test_size'],
+                        'random_seed': config['modeling']['random_seed'],
+                        'stratify_by': config['modeling']['stratify_by']
                 }
             )
 
