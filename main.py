c0e60894590712e24d6f97dd696ab4e79a35a646
peter <pjkrupa@proton.me>
2025-06-14 12:44:51 +0200

    changed sample parameter in get_data to sample.csv

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def go(config: DictConfig):
                 env_manager="conda",
                 parameters={
                     "sample": config["etl"]["sample"],
-                    "artifact_name": config["etl"]["sample"],
+                    "artifact_name": "sample.csv",
                     "artifact_type": "raw_data",
                     "artifact_description": "Raw file as downloaded"
                 },
